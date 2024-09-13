dbfd0210b382f3bdc6036ecf966990535412628b
vio <viocamachomacedo@crackthecode.la>
2024-09-13 12:00:44 -0500

    resultado

--- assets/js/resultado.js
+++ assets/js/resultado.js
@@ -1,27 +1,38 @@
+// Obtén el puntaje del almacenamiento local
 var puntaje = localStorage.getItem('puntaje');
 var resultadoText = '';
+var maxPuntaje = 20; // Puntaje máximo posible
+var porcentaje = ((puntaje / maxPuntaje) * 100).toFixed(0); // Calcula el porcentaje
 
+// Define el texto del resultado basado en el puntaje
 if (puntaje >= 20) {
-  resultadoText = "Wow! lo hiciste excelente, seguro repasas a menudo";
+  resultadoText = "¡Wow! Lo hiciste excelente, seguro repasas a menudo";
 } else if (puntaje > 15) {
-  resultadoText = "Estas por buen camino, !Muy bien :D!";
+  resultadoText = "¡Estás por buen camino! Muy bien :D";
 } else if (puntaje > 11) {
-  resultadoText = "Puedes mejorar, !Tu puedes!";
+  resultadoText = "Puedes mejorar, ¡Tú puedes!";
 } else {
-  resultadoText = "Oh bueno... aqui te podemos ayudar a superarte, no te rindas";
+  resultadoText = "Oh bueno... aquí te podemos ayudar a superarte, no te rindas";
 }
 
-document.getElementById('resultado').innerHTML = `Tu puntaje es de ${puntaje} , ${resultadoText}.`;
+// Muestra el porcentaje en el círculo
+var resultadoElement = document.getElementById('resultado');
+resultadoElement.innerHTML = `${porcentaje}%`;
 
-function si(){
-    window.location.href = '../aprende_s.html'
-    
+// Actualiza el fondo del círculo según el porcentaje
+document.querySelector('.resultado::before').style.backgroundImage = `conic-gradient(#00ff00 ${porcentaje}%, transparent ${porcentaje}% 100%)`;
+
+// Muestra el puntaje y el mensaje en la burbuja de diálogo de Robotín
+document.getElementById('robot-dialog').innerHTML = `Tu puntaje es ${puntaje}. ${resultadoText}`;
+
+function si() {
+    window.location.href = 'aprende_s.html';
 }
 
-function maso(){
-    window.location.href = '../aprende_m.html'
+function maso() {
+    window.location.href = 'aprende_m.html';
 }
 
-function no(){
-  window.location.href = '../aprende_n.html'
+function no() {
+    window.location.href = 'aprende_n.html';
 }
